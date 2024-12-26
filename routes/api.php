02adf35ce84868dc4c0db902927749a060014371
Ali-codingg <alikheireddinee@outlook.com>
2024-12-26 16:50:55 +0200

    add import

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\NotificationsController;
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
